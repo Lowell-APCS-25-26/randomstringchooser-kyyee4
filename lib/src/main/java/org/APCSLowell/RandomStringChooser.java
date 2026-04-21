dde27c4c118f433a65d708295c51f8fa9cdccbd5
Kyle Yee <kyyee4@gmail.com>
2026-04-21 17:17:07 +0000

    4/21 done

--- lib/src/main/java/org/APCSLowell/RandomStringChooser.java
+++ lib/src/main/java/org/APCSLowell/RandomStringChooser.java
@@ -1,4 +1,20 @@
 package org.APCSLowell;
 
+import java.util.ArrayList;
+
 // FRQ: https://secure-media.collegeboard.org/digitalServices/pdf/ap/ap16_frq_computer_science_a.pdf
-public class RandomStringChooser {}
+public class RandomStringChooser {
+    private ArrayList<String> ArrL;
+    public RandomStringChooser(String[] arr){
+        ArrL = new ArrayList<String>();
+        for(int i = 0; i<arr.length;i++){
+            ArrL.add(arr[i]);
+        }
+    }
+    public String getNext(){
+        if (ArrL.size() > 0){
+            return ArrL.remove((int)(Math.random() * ArrL.size()));
+        }
+        return "None";
+    }
+}
